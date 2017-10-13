ed92072875c095d629c3e588ced36c614023c75e
Vladimir Pryahin <pryahin.v@gmail.com>
2017-10-13 19:56:38 +0300

    Test ForumController

--- src/main/java/Database/Controllers/ForumController.java
+++ src/main/java/Database/Controllers/ForumController.java
@@ -1,5 +1,6 @@
 package Database.Controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,20 @@ public class ForumController {
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public String createForum() {
         return "Forum created";
+    }
+
+    @RequestMapping(value = "/{slug}/create", method = RequestMethod.POST)
+    public String slugCreate(@PathVariable(name = "slug") String slug) {
+        return slug+" - created";
+    }
+
+    @RequestMapping(value = "{slug}/details", method = RequestMethod.GET)
+    public String slugDetails(@PathVariable(name = "slug") String slug) {
+        return slug+": ...{details}";
+    }
+
+    @RequestMapping(value = "{slug}/users", method = RequestMethod.GET)
+    public String slugUsers(@PathVariable(name = "slug") String slug) {
+        return slug+" - users: ...";
     }
 }
