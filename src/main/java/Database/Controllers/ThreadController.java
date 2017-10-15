f37218c8416239f1864e6ef842be5f49f2c3b5e2
Vladimir Pryahin <pryahin.v@gmail.com>
2017-10-15 16:09:54 +0300

    Create post

--- src/main/java/Database/Controllers/ThreadController.java
+++ src/main/java/Database/Controllers/ThreadController.java
@@ -1,19 +1,71 @@
 package Database.Controllers;
 
+import Database.DAO.PostDAO;
+import Database.DAO.ThreadDAO;
+import Database.DAO.UserDAO;
+import Database.Models.ErrorModel;
+import Database.Models.PostModel;
+import Database.Models.ThreadModel;
+import Database.Models.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/thread/")
 @RestController
 public class ThreadController {
 
+    private ThreadDAO threadDAO;
+    private PostDAO postDAO;
+    private UserDAO userDAO;
+
+    @Autowired
+    public ThreadController(ThreadDAO threadDAO, PostDAO postDAO, UserDAO userDAO) {
+        this.threadDAO = threadDAO;
+        this.postDAO = postDAO;
+        this.userDAO = userDAO;
+    }
+
     @RequestMapping(value = "/{slug_or_id}/create", method = RequestMethod.POST)
-    public ResponseEntity createPost(@PathVariable(name="slug_or_id") String slug) {
-        return ResponseEntity.status(HttpStatus.CREATED).body("Post in "+slug+" created");
+    public ResponseEntity createPost(@PathVariable(name="slug_or_id") String slug, @RequestBody List<PostModel> posts) {
+        try {
+            ThreadModel thread = threadDAO.getThreadBySlugOrId(slug);
+            if (thread == null) {
+                ErrorModel error = new ErrorModel();
+                error.setMessage("Can't find thread " + slug);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+            }
+
+            for (PostModel post : posts ) {
+                UserModel user = userDAO.getUser(thread.getAuthor());
+                if (user == null) {
+                    ErrorModel error = new ErrorModel();
+                    error.setMessage("Can't find user with nickname" + thread.getAuthor());
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+                }
+
+                if (post.getParent() != 0) {
+                    PostModel parent = postDAO.getPost(post.getParent());
+                    if (parent == null) {
+                        ErrorModel error = new ErrorModel();
+                        error.setMessage("Can't find parent with if " + post.getParent());
+                        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+                    }
+                }
+
+                post.setForum(thread.getForum());
+                post.setThread(thread.getId());
+            }
+
+            postDAO.createPost(posts);
+            return ResponseEntity.status(HttpStatus.CREATED).body(posts);
+        } catch (DuplicateKeyException ex) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("conflict ...");
+        }
     }
 
     @RequestMapping(value = "/{slug_or_id}/details", method = RequestMethod.GET)
